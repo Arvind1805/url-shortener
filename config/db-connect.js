17280bc69873709b737c20d227b5b09ea5f96594
Arvind1805 <varvind1805@gmail.com>
2025-08-05 11:17:54 +0530

    modified database code

--- config/db-connect.js
+++ config/db-connect.js
@@ -1,26 +1,32 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const connectToDB = async()=>{
+export const connectToDB = async () => {
+  await mongoose.connect(process.env.MONGODB_URI + process.env.MONGODB_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
-  await mongoose.connect("mongodb://localhost:27017/url-db")  //connection url + database name 
+  console.log("✅ Connected to MongoDB");
 
-  mongoose.set("debug",true);  // displays the background work done by mongoose methods inorder perform curd operations (uses mongodb operations only)
-  const userSchema = mongoose.Schema({
-    url : {type : String, required : true},
-    shortCode : {type : String, required : true}
-    // createdAt : {type : Date, default : Date.now}  instead of both middlewares and this createdAt we can upload timestamps
-  }
-  )
-  const collection = mongoose.model("url-link",userSchema)  
+  mongoose.set("debug", true);
+
+  const userSchema = new mongoose.Schema({
+    url: { type: String, required: true },
+    shortCode: { type: String, required: true }
+  }, { timestamps: true });
+
+  const collection = mongoose.model("url-link", userSchema);
   return collection;
-
 }
+
                                                            
 // ,{
 //   timestamps : true     => this is used to set createdAt and updatedAt time 
 // }
 
-//using middleware to automate the updation of any value or any operation 
+// using middleware to automate the updation of any value or any operation 
 
 // userSchema.pre(['updateOne'],function(next){
 //     this.set({createdAt : Date.now()})
